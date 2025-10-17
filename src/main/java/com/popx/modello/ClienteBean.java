cbbfaf915a9d23bba9d4d260e9e41eae364d176a
Daniele Pio Scaparra <162312256+dscap02@users.noreply.github.com>
2025-10-17 17:26:18 +0200

    docs: added JML specs to ClientBean
    Added invariants, pre/post conditions for OpenJML verification.

--- src/main/java/com/popx/modello/ClienteBean.java
+++ src/main/java/com/popx/modello/ClienteBean.java
@@ -1,10 +1,28 @@
 package com.popx.modello;
 
-
+/*@
+  @ also
+  @ invariant true;
+@*/
 public class ClienteBean extends UserBean {
+
+    /*@
+      @ ensures this.getUsername() == null
+      @      && this.getEmail() == null
+      @      && this.getPassword() == null
+      @      && this.getRole() == null;
+    @*/
     public ClienteBean() {}
 
+    /*@
+      @ requires username != null && email != null && password != null && role != null;
+      @ ensures this.getUsername().equals(username)
+      @      && this.getEmail().equals(email)
+      @      && this.getPassword().equals(password)
+      @      && this.getRole().equals(role);
+    @*/
     public ClienteBean(String username, String email, String password, String role) {
         super(username, email, password, role);
     }
 }
+
